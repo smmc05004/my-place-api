87c21230e3b653875e0191df399d2e49413c81de
front_이석화 <shlee@cashwalk.io>
2022-05-25 14:31:04 +0900

    테스트 코드 제거

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TestController } from './test/test.controller';
-import { TestService } from './test/test.service';
 import { FoodModule } from './food/food.module';
 
 @Module({
   imports: [FoodModule],
-  controllers: [AppController, TestController],
-  providers: [AppService, TestService ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
