a5118ea831c1a51a759099048818388c998654c2
front_이석화 <shlee@cashwalk.io>
2022-05-30 14:08:16 +0900

    더미데이터 제거

--- src/food/service/food.service.ts
+++ src/food/service/food.service.ts
@@ -1,7 +1,6 @@
 import { Get, Injectable } from '@nestjs/common';
 import { Food } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { foodList } from '../foodList';
 
 // 0 -> 방문 예정, 1 -> 방문지
 @Injectable()
