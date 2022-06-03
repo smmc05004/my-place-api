09b1f23a600d8c35a0bd0f84ff6329e4839b193a
front_이석화 <shlee@cashwalk.io>
2022-06-03 16:19:41 +0900

    MOD: food 요청시 attach도 가져오도록 쿼리 수정

--- src/food/service/food.service.ts
+++ src/food/service/food.service.ts
@@ -4,69 +4,72 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { FoodCreateDTO } from '../dto/foodCreate.dto';
 
 interface FoodCountProps {
-  category: number;
+	category: number;
 }
 interface FoodListProps extends FoodCountProps {
-  page: number;
+	page: number;
 }
 
 // 0 -> 방문 예정, 1 -> 방문지
 @Injectable()
 export class FoodService {
-  constructor(private prisma: PrismaService) {}
+	constructor(private prisma: PrismaService) {}
 
-  async getFoods({ category, page }: FoodListProps): Promise<Food[]> {
-    const rowSize = 10;
+	async getFoods({ category, page }: FoodListProps): Promise<Food[]> {
+		const rowSize = 10;
 
-    const result = this.prisma.food.findMany({
-      skip: (page - 1) * rowSize,
-      take: rowSize,
-      where: {
-        category,
-        writerId: 1,
-      },
-      orderBy: {
-        id: 'desc',
-      },
-      include: {
-        writer: true,
-      },
-    });
-    return result;
-  }
+		const result = this.prisma.food.findMany({
+			skip: (page - 1) * rowSize,
+			take: rowSize,
+			where: {
+				category,
+				writerId: 1,
+			},
+			orderBy: {
+				id: 'desc',
+			},
+			include: {
+				writer: true,
+			},
+		});
+		return result;
+	}
 
-  async getFoodsCount({ category }: FoodCountProps): Promise<number> {
-    const result = await this.prisma.food.count({
-      where: {
-        category,
-        writerId: 1,
-      },
-    });
+	async getFoodsCount({ category }: FoodCountProps): Promise<number> {
+		const result = await this.prisma.food.count({
+			where: {
+				category,
+				writerId: 1,
+			},
+		});
 
-    return result;
-  }
+		return result;
+	}
 
-  async getFoodById(id: number): Promise<Food | null> {
-    const food = await this.prisma.food.findUnique({
-      where: {
-        id,
-      },
-    });
+	async getFoodById(id: number): Promise<Food | null> {
+		const food = await this.prisma.food.findUnique({
+			where: {
+				id,
+			},
+			include: {
+				attach: true,
+			},
+		});
 
-    return food;
-  }
+		return food;
+	}
 
-  async addFood(data: FoodCreateDTO): Promise<Food> {
-    return await this.prisma.food.create({
-      data: {
-        ...data,
-        attach: {
-          create: {
-            type: 'aaaa',
-            name: 'bbbb',
-          },
-        },
-      },
-    });
-  }
+	async addFood(data: FoodCreateDTO): Promise<Food> {
+		return await this.prisma.food.create({
+			data: {
+				...data,
+				attach: {
+					create: {
+						type: 'aaaa',
+						name: 'bbbb',
+					},
+				},
+			},
+		});
+	}
 }
