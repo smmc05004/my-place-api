c30fa58fa86367869203717c1386ac2f96f9ec67
front_이석화 <shlee@cashwalk.io>
2022-06-15 17:02:15 +0900

    DEL: 불필요한 import 제거

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { PrismaService } from 'src/prisma/prisma.service';
-import { UserService } from 'src/user/service/user.service';
 import { UserModule } from 'src/user/user.module';
-import { jwtContants } from './constant/constant';
 import { AuthController } from './controller/auth.controller';
 import { AuthService } from './service/auth.service';
 import { JwtStrategy } from './service/jwt.strategy';
